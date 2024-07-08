ffcabfcd5a28287247f30a649c91e0353ff9445b
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-07 23:13:44 -0500

    Subo el arreglo 3.

---
+++ arregloo3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    float TMedia, TMin[7], TMax[7];
+    int diaMTemp = 0, In = 0, Tx;
+    for (int i = 0; i < 7; ++i) {
+        cout << "Ingrese la temperatura minima del dia " << i + 1 << ": "; cin>> TMin[i];
+        cout << "Ingrese la temperatura maxima del dia " << i + 1 << ": "; cin>> TMax[i];
+    }
+    cout << "\nLa temperatura media de cada dia es:\n";
+    for (int i = 0; i < 7; ++i) {
+        TMedia = (TMin[i] + TMax[i]) / 2;
+        cout << "Dia " << i + 1 << ": " << TMedia<< "C" << endl;
+    
+        if (TMin[i] < TMin[diaMTemp]) {
+            diaMTemp = i;
+        }
+    }
+    cout << "La menor temperatura minima fue el dia " << diaMTemp + 1 << " con " << TMin[diaMTemp] << "C\n";
+    cout << "Ingrese una temperatura para buscar dias que coincidan con esa temperatura maxima: "; cin >> Tx;
+   
+    for (int i = 0; i < 7; ++i) {
+        if (TMax[i] == Tx) {
+            cout << "El dia " << i + 1 << " tiene una temperatura maxima de " << Tx << "C.\n";
+            In = 1; 
+        }   
+    }
+    if (In == 0) {
+        cout << "No hay ni un dia que coincida con una temperatura maxima de " << Tx << "C.\n";
+    }
+
+    return 0;
+}
+   
