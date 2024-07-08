b1ada169a8696f95810df45980bb5a7f616f1530
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-08 00:09:05 -0500

    Actualizacion del ejercicio 4: agregue una funcion para los multiplos y otra que salga los resultados.

--- arregloo4.cpp
+++ arregloo4.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void arrMultiplos(int arreglo[], int a, int b);
+
+void arrResultados(int arreglo[], int x);
+
 int main() {
-	int tm, nr, i;
-	int arreglo[i];
-	cout << "Ingrese el tamano que desea para el arreglo: "; cin >> tm;
-	cout << "Ingrese el numero deseado para obtener sus multiplos: "; cin >> nr;
-	int arr[tm];
-	for (int i = 0; i < tm; i++) {
-    arreglo[i] = nr * (i + 1);
-	}
-	cout << "El arreglo con multiplos de " << nr << " son: \n";
-	for (int i = 0; i < tm; i++) {
-    cout << arreglo[i] <<endl;
-	}
-  	return 0;
+  int tm, nr;
+  cout << "Ingrese el tamano que desea para el arreglo: "; cin >> tm;
+  cout << "Ingrese el numero deseado para obtener sus multiplos: "; cin >> nr;
+  int arr[tm];
+  arrMultiplos(arr, tm, nr);
+  cout << "El arreglo con multiplos de " << nr << " son: \n";
+  arrResultados(arr, tm);
+  return 0;
+}
+
+void arrMultiplos(int arreglo[], int a, int b) {
+  for (int i = 0; i < a; i++) {
+    arreglo[i] = b * (i + 1);
+  }
+}
+
+void arrResultados(int arreglo[], int x) {
+  for (int i = 0; i < x; i++) {
+    cout << arreglo[i] << " "<<endl;
+  }
 }
