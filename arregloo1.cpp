4f5c596387aa20d1f8d5083f106813201949b690
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-07 20:40:12 -0500

    Actualizacion del ejercicio 1: arregle un error que habia en la parte del for, declarando que la variable c es igual a 0, y ahora si da bien el procentaje.

--- arregloo1.cpp
+++ arregloo1.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 int main (){
 
-	int n, p=0, s=0, c=0, i;
+	int n, p=0, s=0, c, i;
 	float arreglo[30];
 	
 	cout<<"Elementos del arreglo (max 30): "; cin>>n;
 
-	for(i=0;i<n;i++){
+	for(c=0,i=0;i<n;i++){
 		cout<<"ingrese los numeros #:"<<i+1<<endl; cin>>arreglo[i];
 		if (arreglo[i]==0){
-			c = c + 1;
+			c++;
 		} else {
 			s = s + arreglo[i];
 		}
 		
 	}
-	p=(c/n)*100;
+	p=(c*100/n);
 	cout<<"La sumatoria es: "<<s<<endl;
-	cout<<"El porcentaje es: "<<p<<endl;
+	cout<<"El porcentaje es: "<<p<<"%"<<endl;
 	return 0;
-	}
+	} 
