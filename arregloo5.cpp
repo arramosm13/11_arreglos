b4e74c40b1ad152b1204173c21005a9780656b3d
arramosm13 <arramosm@unjbg.edu.pe>
2024-07-08 01:44:12 -0500

    Actualizacion del ejercicio 5: agregue un if en cada que pide la cantidad de estudiantes, y que solo pueda ingresar numeros positivos, porque no hay negativos en la cantidad de estudiantes.

--- arregloo5.cpp
+++ arregloo5.cpp
@@ -4,22 +4,27 @@ using namespace std;
 int main() {
     int nFP, nPG, mn=0;
     cout << "Ingrese el numero de estudiantes que hay en la clase de Fundamentos de Programacion: "; cin >> nFP;
+    if (nFP < 0) {
+        cout << "El numero de estudiantes no puede ser uno negativo." << endl;
+    }
     cout << "Ingrese los nombres de cada estudiante de la clase de Fundamentos de Programacion:\n"; 
 	string funda[nFP];
     for(int i = 0; i < nFP; ++i) {
         cin >> funda[i];
     }
     cout << "Ingrese el numero de estudiantes que hay en la clase de Programacion Grafica: "; cin >> nPG;
+    if (nPG < 0) {
+        cout << "El numero de estudiantes no puede ser uno negativo." << endl;
+    }
     cout << "Ingrese los nombres de cada estudiante de la clase de Programacion Grafica:\n";  
 	string progra[nPG];
     for(int i = 0; i < nPG; ++i) {
         cin >> progra[i];
     }
-
     cout << "Estudiantes con mismo nombre en ambas asignaturas:\n";
     for(int i = 0; i < nFP; ++i) {
-        for(int j = 0; j < nPG; ++j) {
-            if(funda[i] == progra[j]) {
+        for(int k = 0; k < nPG; ++k) {
+            if(funda[i] == progra[k]) {
                 cout << funda[i] << "\n";
                 ++mn;
                 break; 
@@ -29,3 +34,4 @@ int main() {
     cout << "Total de estudiantes con mismo nombre: " <<mn << "\n";
     return 0;
 }
+
